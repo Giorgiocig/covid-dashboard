196ebbbdcc447b974ac3ecc8406ca1354bab6f95
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-07 15:39:03 +0200

    Implement VerticalBarChart component

---
+++ src/components/graphs/VerticalBarChart.tsx
@@ -0,0 +1,44 @@
+import Plot from "react-plotly.js";
+
+export default function VerticalBarChart({
+  labels,
+  data,
+}: {
+  labels: string[];
+  data: number[];
+}) {
+  return (
+    <>
+      <Plot
+        data={[
+          {
+            type: "bar",
+            x: labels,
+            y: data,
+            marker: {
+              color: "#1E3A8A",
+            },
+          },
+        ]}
+        layout={{
+          width: 500,
+          height: 500,
+          title: {
+            text: "Top 10 nations per number of deaths",
+          },
+          xaxis: {
+            title: { text: "Nations", standoff: 15 },
+          },
+          yaxis: {
+            title: {
+              text: "Number of Deaths",
+              standoff: 15, // <-- Aggiunge distanza tra il titolo e i tick dell’asse Y
+            },
+            tick0: 0,
+            dtick: 30000,
+          },
+        }}
+      />
+    </>
+  );
+}
